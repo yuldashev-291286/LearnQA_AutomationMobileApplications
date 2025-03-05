f5b20ea13fb21c1dec13aef52bf9b764f66f5418
yuldashev-291286 <56519643+yuldashev-291286@users.noreply.github.com>
2025-03-05 19:08:07 +0300

    Занятие 1. Тест 2.

--- src/main/java/MainClass.java
+++ src/main/java/MainClass.java
@@ -1,5 +1,15 @@
 public class MainClass {
 
+    // Сделать в классе MainClass приватное поле класса, которое равно 20 (назвать: class_number).
+    private int class_number = 20;
+
+    // Сделать в классе MainClass публичный метод (getClassNumber), который эту переменную возвращает.
+    public int getClassNumber() {
+
+        return this.class_number;
+    }
+
+    // В классе MainClass сделать метод, который возвращает число 14 (назвать: getLocalNumber).
     public int getLocalNumber(){
 
         return 14;
--- src/main/java/MainClassTest.java
+++ src/main/java/MainClassTest.java
@@ -1,3 +1,4 @@
+import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
@@ -7,11 +8,21 @@ public class MainClassTest {
 
     MainClass mainClass = new MainClass();
 
+    // В классе MainClassTest написать тест, проверяющий, что метод getLocalNumber возвращает число 14 (назвать: testGetLocalNumber).
     @Test
     public void testGetLocalNumber(){
 
         assertEquals(14, this.mainClass.getLocalNumber(), "Возвращаемое число не равно 14");
 
+    }
+
+    // В классе MainClassTest написать тест (назвать testGetClassNumber), который проверяет, что метод getClassNumber возвращает число больше 45.
+    @Test
+    public void testGetClassNumber(){
+
+        assertTrue(this.mainClass.getClassNumber() > 45, "Возвращаемое число меньше 45");
 
     }
+
+
 }
